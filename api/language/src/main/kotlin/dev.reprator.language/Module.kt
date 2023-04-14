e1f9340e739be75602e64563922a37a4e6e85cd7
vikramsingh <mr.vikram004@gmail.com>
2023-04-14 10:46:45 +0400

    qualifier added

--- api/language/src/main/kotlin/dev.reprator.language/Module.kt
+++ api/language/src/main/kotlin/dev.reprator.language/Module.kt
@@ -11,13 +11,17 @@ import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import dev.reprator.language.data.mapper.LanguageResponseMapper
+import org.koin.core.module.dsl.withOptions
+import org.koin.core.qualifier.named
 
 /**
  * The container to inject all dependencies from the language module.
  */
+private const val KOIN_NAMED_MAPPER_LANGUAGE = "languageMapper"
+
 val languageModule = module {
-    singleOf(::LanguageResponseMapper) bind Mapper::class
-    single<LanguageRepository> { LanguageRepositoryImpl(get()) }
+    singleOf(::LanguageResponseMapper).withOptions {qualifier = named(KOIN_NAMED_MAPPER_LANGUAGE) } bind Mapper::class
+    single<LanguageRepository> { LanguageRepositoryImpl(get(qualifier = named(KOIN_NAMED_MAPPER_LANGUAGE))) }
     singleOf(::LanguageFacadeImpl) bind LanguageFacade::class
     single { LanguageControllerImpl(get()) } bind LanguageController::class
 }
